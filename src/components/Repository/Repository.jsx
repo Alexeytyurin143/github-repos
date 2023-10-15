719376ed757b79b3c300490be952fcaad5f54bbd
Alexey Tyurin <tyurin.alexey.2005@yandex.ru>
2023-10-15 14:20:51 +0400

    Стили для компоненты репозитория

--- src/components/Repository/Repository.jsx
+++ src/components/Repository/Repository.jsx
@@ -2,18 +2,18 @@ import React from 'react'
 import styles from './Repository.module.scss'
 
 const Repository = ({ repository }) => {
-    return (
-        <div className={styles.repository}>
-            <div className={styles.header}>
-                <div className={styles.name}>{repository.name}</div>
-                <div className={styles.stars}>{repository.stargazers_count}</div>
-            </div>
-            <div className={styles.lastCommit}>{repository.updated_at}</div>
-            <a href={repository.html_url} className={styles.link}>
-                Ссылка на репозиторий
-            </a>
-        </div>
-    )
+	return (
+		<div className={styles.repository}>
+			<a href={repository.html_url} className={styles.name}>
+				Репозиторий {repository.name}
+			</a>
+			<div className={styles.description}>{repository.description}</div>
+			<div className={styles.footer}>
+				<div className={styles.lastCommit}>Последний коммит: {repository.updated_at}</div>
+				<div className={styles.stars}>Количество звёзд: {repository.stargazers_count}</div>
+			</div>
+		</div>
+	)
 }
 
 export default Repository
