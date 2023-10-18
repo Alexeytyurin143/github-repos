6f3d3c4f19f6033ee6eb58fb31fe61a096c7b274
Alexey Tyurin <tyurin.alexey.2005@yandex.ru>
2023-10-18 18:51:20 +0400

    Очистка App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,15 @@
-import { useState } from 'react'
-import './App.css'
-import { useDispatch, useSelector } from 'react-redux'
+import './App.scss'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Main from './components/Main/Main'
 
 function App() {
-    const dispatch = useDispatch()
-    const count = useSelector((state) => state.repos.count)
-    const handleClick = () => {}
-
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' Component={Main} />
-            </Routes>
-        </BrowserRouter>
-    )
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' Component={Main} />
+			</Routes>
+		</BrowserRouter>
+	)
 }
 
 export default App
