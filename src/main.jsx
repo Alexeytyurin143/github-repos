94edea6b7e342cbe3ccb2dfb2c9f603be291c8f1
Alexey Tyurin <tyurin.alexey.2005@yandex.ru>
2023-10-18 18:50:52 +0400

    Reset

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './reset.scss'
+import './index.scss'
 import { Provider } from 'react-redux'
 import { store } from './reducers/index.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <App />
-        </Provider>
-    </React.StrictMode>
+	<React.StrictMode>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</React.StrictMode>
 )
