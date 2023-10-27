1fcdce99274985ec99b37acbb1cbe312f627e990
Alexey Tyurin <tyurin.alexey.2005@yandex.ru>
2023-10-27 21:36:39 +0400

    Удаление импортов

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,7 +1,6 @@
 import styles from './Main.module.scss'
 import Repositories from '../Repositories/Repositories'
 import SearchForm from '../SearchForm/SearchForm'
-import { useState } from 'react'
 
 const Main = () => {
 	return (
