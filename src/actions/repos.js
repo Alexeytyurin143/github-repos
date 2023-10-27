dbe63c60d3989075c077eeab9cd837de8c89930f
Alexey Tyurin <tyurin.alexey.2005@yandex.ru>
2023-10-27 22:09:40 +0400

    Добавление токена

--- src/actions/repos.js
+++ src/actions/repos.js
@@ -2,16 +2,26 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const reposApi = createApi({
 	reducerPath: 'reposApi',
-	baseQuery: fetchBaseQuery({ baseUrl: 'https://api.github.com/' }),
+	baseQuery: fetchBaseQuery({
+		baseUrl: 'https://api.github.com/',
+		prepareHeaders: (headers) => {
+			headers.set('Authorization', 'Bearer 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')
+			headers.set('X-GitHub-Api-Version', '2022-11-28')
+			return headers
+		},
+	}),
 	endpoints: (builder) => ({
-		getRepos: builder.query({
-			query: ({ searchQuery, currentPage }) => {
-				if (!searchQuery) {
-					searchQuery = 'stars:>1'
-				}
-				return `search/repositories?q=${searchQuery}&sort=stars&per_page=10&page=${currentPage}`
+		getRepos: builder.query(
+			{
+				query: ({ searchQuery, currentPage }) => {
+					if (!searchQuery) {
+						searchQuery = 'stars:>1'
+					}
+					return `search/repositories?q=${searchQuery}&sort=stars&per_page=10&page=${currentPage}`
+				},
 			},
-		}),
+			Headers
+		),
 	}),
 })
 
